e00defaaa654745516dbfedd62cd246bf1229b63
Thiago André Cardoso Silva <33556919+thiagoandrecardoso@users.noreply.github.com>
2024-10-05 18:48:06 -0300

    initial commit

---
+++ src/test/java/who/programador/mm/patientmodule/PatientModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package who.programador.mm.patientmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
